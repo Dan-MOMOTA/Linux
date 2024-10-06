02e6e04953c267ebb0a0f8bb0c71dcdad5236e7b
Dan-MOMOTA <142721494+Dan-MOMOTA@users.noreply.github.com>
2024-10-07 00:34:34 +0800

    modify awk

---
+++ linux_command/grep_sed_awk/awk/awk.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+echo -e "\n========================================="
+echo "1.打印文件file.txt的每一行"
+echo "command:awk '{ print }' ../file.txt"
+awk '{ print }' ../file.txt
+echo -e "\n========================================="
+
